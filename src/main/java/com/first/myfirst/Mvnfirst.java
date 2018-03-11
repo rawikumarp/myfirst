e2147f0eb9b32d7782b036401f22bae8ddc36692
rawikumarp <iamrawikumar@gmail.com>
2018-03-11 12:12:14 +0530

    new files added

---
+++ src/main/java/com/first/myfirst/Mvnfirst.java
@@ -0,0 +1,15 @@
+package com.first.myfirst;
+
+public class Mvnfirst {
+
+	public Mvnfirst() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("practicing");
+		// TODO Auto-generated method stub
+
+	}
+
+}
